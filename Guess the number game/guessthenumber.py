1aa531baf642d20fee5a1077362901ca469ed95a
billavamsikrishna <69465140+billavamsikrishna@users.noreply.github.com>
2020-08-10 18:38:31 +0530

    Add files via upload

--- Guess the number game/guessthenumber.py
+++ Guess the number game/guessthenumber.py
@@ -1,1 +1,18 @@
-
+import random
+print("Hello..! what's your name..?")
+name = input()
+print('well,'+name+ ' I am thinking of a number between range of 1 to 20..')
+secret_number = random.randint(1,20)
+for i in range(1,6):
+    print('Take a Guess:')
+    guess = int(input())
+    if guess > secret_number:
+        print('Sorry..! your guess is too high ' +name+'.')
+    elif guess < secret_number:
+        print('Sorry..! your guess is too low '+name+'.')
+    else:
+        break
+if guess == secret_number:
+    print('Good job, '+name+'! you guessed my number in '+str(i)+'guesses.')
+else:
+    print('Nope, you exceeded guess times. My number is '+str(secret_number)+'.')
